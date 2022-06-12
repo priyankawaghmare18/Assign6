d440df160abbc01e88ae7e2f34309424df1175a8
priyankawaghmare18 <63039867+priyankawaghmare18@users.noreply.github.com>
2022-06-12 17:06:04 +0530

    src

---
+++ src_3/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template:`<h1>Marvellous Infosystems</h1>
+  <input type="text"/>`,
+  styles:[`h1{ color: blue;}`]
+})
+export class AppComponent {
+  title = 'WebApps';
+}
